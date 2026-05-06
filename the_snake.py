353ba10affeca6ad09f30ed3029703bca2af326e
Никита Антонов <tsotikin@gmail.com>
2026-05-06 16:50:09 +0300

    Добавлен метод __init__ в класс Apple. Исправлены методы randomize_position и draw класса Apple, и метод __init__ класса GameObject. Изменены докстринги соответственно.

--- the_snake.py
+++ the_snake.py
@@ -39,65 +39,38 @@ pygame.display.set_caption('Змейка')
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
 class GameObject:
-    """
-    Базовый класс, от которого наследуются все объекты.
-    Содержит общие атрибуты: позиция и цвет.
-    """
+    """Базовый класс для всех игровых объектов."""
 
     def __init__(self, position=None, body_color=None):
-        """
-        Конструктор базового игрового объекта.
-        Аргументы: position (координаты), body_color (цвет).
-        """
-        if position is None:
-            self.position = (320, 240)
-        else:
-            self.position = position
+        """Инициализирует базовый игровой объект."""
+        self.position = position
+        self.body_color = body_color
 
     def draw(self, surface):
-        """
-        Абстрактный метод для отрисовки объекта на экране.
-        Аргумент: surface(поверхность, на которой рисуем).
-        """        
+        """Абстрактный метод для отрисовки объекта."""
         pass
 
+
 class Apple(GameObject):
-    """
-    Класс Apple. Наследуется от GameObject.
-    Появлется в случайном месте поля.
-    """    
+    """Класс яблока, наследуется от GameObject."""
 
-    super.__init__(position=None, body_color=APPLE_COLOR)
+    def __init__(self):
+        """Инициализирует яблоко со случайной позицией."""
+        super().__init__(body_color=APPLE_COLOR)
+        self.randomize_position()
 
-    self.radomize_position()
-
-    def radomize_position(self):
-        """
-        Устанавливает случайные координаты для яблока.
-        """
-        max_x = 640 - 20
-        max_y = 480 - 20
-
-        x.random.randrage(0, max_x + 1, 20)
-        y.random.randrage(0, max_y + 1, 20)
-
+    def randomize_position(self):
+        """Устанавливает случайную позицию яблока на игровом поле."""
+        x = randint(0, GRID_WIDTH - 1) * GRID_SIZE
+        y = randint(0, GRID_HEIGHT - 1) * GRID_SIZE
         self.position = (x, y)
 
     def draw(self, surface):
-        """
-        Отрисовывает яблоко на игровом поле. 
-        """
-
-        rect = pygame.Rect(
-            self.position[0],
-            self.position[1],
-            20,
-            20
-        )
-
+        """Отрисовывает яблоко на игровой поверхности."""
+        rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(surface, self.body_color, rect)
+        pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
 
 
 def main():
